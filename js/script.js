f80b02528d66247448d0ced46eb4d410d458c70b
Yevhenii <evhengalibarenko@gmail.com>
2018-09-15 20:45:31 +0300

    footer/slider

---
+++ js/script.js
@@ -0,0 +1,7 @@
+$('.slider').slick({
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    prevArrow:"<img class='a-left control-c prev slick-prev' src='../img/slider/prev-arrow.png'>",
+    nextArrow:"<img class='a-right control-c next slick-next' src='../img/slider/next-arrow.png'>"
+  }
+)
